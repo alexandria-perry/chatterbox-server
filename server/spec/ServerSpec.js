1a739da8b4cca91644e453d53ec807f0bb16cf48
alexandria-perry <aeperry22@gmail.com>
2018-11-30 17:22:02 -0800

    Make progress on handle request functionality

--- server/spec/ServerSpec.js
+++ server/spec/ServerSpec.js
@@ -63,7 +63,10 @@ describe('Node Server Request Listener Function', function() {
 
     // Testing for a newline isn't a valid test
     // TODO: Replace with with a valid test
-    // expect(res._data).to.equal(JSON.stringify('\n'));
+    expect(res._data).to.equal(JSON.stringify({
+      username: 'Jono',
+      text: 'Do my bidding!'
+    }));
     expect(res._ended).to.equal(true);
   });
 
